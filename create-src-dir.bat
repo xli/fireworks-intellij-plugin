db00f01417e563d5f974a741532d1764cee3f204
swing1979 <swing1979@0474f589-cd1e-0410-8b18-53dfffbac92a>
2006-10-03 11:25:21 +0000

    modify create src dir bat file, rebuild dontmakemetest-src.zip, add dependent libs 'turtlemock' and 'ant' into it.
    git-svn-id: https://dontmakemetest.googlecode.com/svn/trunk@10 0474f589-cd1e-0410-8b18-53dfffbac92a

--- create-src-dir.bat
+++ create-src-dir.bat
@@ -1,6 +1,9 @@
 md dontmakemetest-src
 md dontmakemetest-src\core
 md dontmakemetest-src\fireworks
+md dontmakemetest-src\dependent-libs
 
 xcopy core dontmakemetest-src\core /E
 xcopy fireworks dontmakemetest-src\fireworks /E
+xcopy "C:\Documents and Settings\xli\.m2\repository\turtlemock\turtlemock\1.0-SNAPSHOT\turtlemock-1.0-SNAPSHOT.jar" dontmakemetest-src\dependent-libs
+xcopy "C:\Documents and Settings\xli\.m2\repository\ant\ant\1.6.5\ant-1.6.5.jar" dontmakemetest-src\dependent-libs
