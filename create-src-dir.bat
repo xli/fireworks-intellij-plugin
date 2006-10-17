72f1b9b787042cba4db5a00d16adc9f9cc93ce4d
swing1979 <swing1979@0474f589-cd1e-0410-8b18-53dfffbac92a>
2006-10-17 08:41:30 +0000

    fix zip dontmakemetest-src.zip error
    git-svn-id: https://dontmakemetest.googlecode.com/svn/trunk@36 0474f589-cd1e-0410-8b18-53dfffbac92a

--- create-src-dir.bat
+++
@@ -1,8 +0,0 @@
-md dontmakemetest-src
-md dontmakemetest-src\core
-md dontmakemetest-src\fireworks
-md dontmakemetest-src\dependent-libs
-
-xcopy core dontmakemetest-src\core /E
-xcopy fireworks dontmakemetest-src\fireworks /E
-xcopy dependent-libs dontmakemetest-src\dependent-libs
